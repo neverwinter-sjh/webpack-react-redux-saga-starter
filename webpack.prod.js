b8c76b4cfd8d428ab8d80c782d78b708b9d31267
심재호 <neverwinter@amorepacific.com>
2020-07-30 18:11:35 +0900

    수정

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
 const webpack = require('webpack');
 const common = require('./webpack.common.js');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = merge(common, {
   // 빌드 환경
